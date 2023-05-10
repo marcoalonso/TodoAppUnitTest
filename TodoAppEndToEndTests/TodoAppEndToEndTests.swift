4abc4b139a810851e6b3b603dda7966c678cc2fb
Marco Alonso Rodriguez Tapia <mrodriguez@alquimiapay.com>
2023-05-10 13:41:08 -0600

    36 Writing Test to Check Duplicate Task Titles are not allowed

--- TodoAppEndToEndTests/TodoAppEndToEndTests.swift
+++ TodoAppEndToEndTests/TodoAppEndToEndTests.swift
@@ -26,6 +26,10 @@ class when_app_is_launched: XCTestCase {
     }
 }
 
+
+
+
+
 class when_user_saves_a_new_task: XCTestCase {
     
     var app: XCUIApplication!
@@ -38,9 +42,7 @@ class when_user_saves_a_new_task: XCTestCase {
     
     override func tearDown() {
         super.tearDown()
-        
         Springboard.deleteApp()
-        
     }
     
     func test_should_be_able_to_display_task_successfully() {
@@ -57,19 +59,45 @@ class when_user_saves_a_new_task: XCTestCase {
         saveTaskButton.tap()
         
         let taskList = app.tables["taskList"]
-        XCTAssertEqual(0, taskList.cells.count)
+        XCTAssertEqual(1, taskList.cells.count)
         
     }
     
-    ///Only execute this function to delete an existing task
-    func test_should_erase_a_task_only_when_there_are_one_or_more_task(){
+    func test_display_error_message_for_duplicate_title_tasks() {
         
-        let tasklistCollectionView = XCUIApplication().collectionViews["taskList"]
-        tasklistCollectionView/*@START_MENU_TOKEN@*/.staticTexts["Medium"]/*[[".cells.staticTexts[\"Medium\"]",".staticTexts[\"Medium\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.swipeLeft()
-        tasklistCollectionView.buttons["Delete"].tap()
         
+        let titleTextField = app.textFields["titleTextField"]
+        titleTextField.tap()
+        titleTextField.typeText("Study SwiftUI and make apps")
+        
+        let saveTaskButton = app.buttons["saveTaskButton"]
+        saveTaskButton.tap()
+        
+        titleTextField.tap()
+        titleTextField.typeText("Study SwiftUI and make apps")
+        
+        saveTaskButton.tap()
+        
+        //If want to know how many tasks there are
+//        let taskList = app.tables["taskList"]
+//        XCTAssertEqual(1, taskList.cells.count)
+        
+        let messageText = app.staticTexts["messageText"]
+        XCTAssertEqual(messageText.label, "Task is already added")
         
     }
+    
+    
+    
+    ///Only execute this function to delete an existing task
+//    func test_should_erase_a_task_only_when_there_are_one_or_more_task(){
+//
+//        let tasklistCollectionView = XCUIApplication().collectionViews["taskList"]
+//        tasklistCollectionView/*@START_MENU_TOKEN@*/.staticTexts["Medium"]/*[[".cells.staticTexts[\"Medium\"]",".staticTexts[\"Medium\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.swipeLeft()
+//        tasklistCollectionView.buttons["Delete"].tap()
+//
+//
+//    }
     
     
     
